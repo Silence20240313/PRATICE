b2f60829ef4492ec018f1e2619317c6e72c682f7
Silence20240313 <liangxiaoli378@gmail.com>
2024-07-10 21:02:44 +0900

    7.10

--- src/com/itheima/day1/PraticeDayOne.java
+++ src/com/itheima/day1/PraticeDayOne.java
@@ -1,4 +1,25 @@
 package com.itheima.day1;
 
 public class PraticeDayOne {
+    public static void main(String[] args) {
+
+
+        System.out.print('中');
+        System.out.print('\n');
+        System.out.print('\t');
+        System.out.print('国');
+
+        int x = 10;
+        System.out.println(x);
+        x = 22;
+        System.out.println(x);
+
+
+        int age = 18;
+        System.out.println(age);
+
+        age=age+10;
+        System.out.println(age);
+
+    }
 }
