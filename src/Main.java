29741f607db08cb4e2cfb9e3ea7a6cd5659b896a
Silence20240313 <liangxiaoli378@gmail.com>
2024-07-10 23:12:47 +0900

    7.10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        double money = 19.5;
+//        System.out.println(money);
+        int i = 1500;
+        byte j = (byte) i;
+        System.out.println(j);
+
+        String rs = "fjskdlfjldsaf";
+       char = 'a'
     }
 }
